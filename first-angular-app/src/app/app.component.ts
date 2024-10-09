744de571744b00b87a584e17881d9b5140309b5a
ui-kenjie <132860983+ui-kenjie@users.noreply.github.com>
2024-10-09 10:20:52 +0800

    'test'

--- first-angular-app/src/app/app.component.ts
+++ first-angular-app/src/app/app.component.ts
@@ -11,5 +11,7 @@ import { HeaderComponent } from './header/header.component';
 })
 export class AppComponent {
   title: string = 'HELLO WORLDS';
+  nickname: any = "kenjie123"
+  try: any = '12aszAAA'
   imgUrl: string = "https://miro.medium.com/v2/resize:fit:1400/1*xvkF4IfpJ65JyYc_xQm6Lw.png";
 }
